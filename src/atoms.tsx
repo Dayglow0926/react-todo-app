ee1a1f1e240790ec97c6d77c7cae8e9f26dcfe1d
Dayglow0926 <dayglow0926@gmail.com>
2023-06-08 23:33:29 +0900

    feat: selector 를 활용한 ToDo 페이지

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,7 +1,21 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import { IToDo } from "./interface/IToDo";
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
